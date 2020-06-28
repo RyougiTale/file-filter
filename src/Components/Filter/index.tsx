3334c90b0cb5cca0ae17480a436bf2ba8538451e
YHevIs <2392022530@qq.com>
2020-06-28 13:52:40 +0800

    筛选按钮基本样式

--- src/Components/Filter/index.tsx
+++ src/Components/Filter/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ListV } from '../ListV'
+import './index.css'
 
 interface Props {
     ButtonsArray: String[],
@@ -7,28 +8,58 @@ interface Props {
 };
 
 interface State {
+    ButtonState: any[]
 };
 
 export class Filter extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
+        this.state = { ButtonState:[] };
     }
-
-    componentWillMount() { }
-
+    //初始化按钮数组
+    init(){
+        let {ButtonsArray,ButtonNum} = this.props;
+        ButtonsArray.map((item:any,v:any)=>{
+            this.state.ButtonState[item] = 0
+        })
+    }
+    componentWillMount() { 
+        this.init();
+    }
     render() {
-        return (<div></div>)
+        let {ButtonsArray,ButtonNum} = this.props;
+        //循环渲染筛选按钮
+        let ButtonDom:any = ButtonsArray.map((item:any,v:any)=>{
+            return <button key={item} onClick={this.clickButton.bind(this,item)} className={this.state.ButtonState[item] === 1 ? "targetButton" : ""}>{item}</button>
+        });
+        return (<div className="buttonList">
+            {ButtonDom}
+        </div>)
     }
 
-    componentDidMount() { }
+    //点击筛选按钮
+    clickButton(buttonName:any):void{
+        switch(this.state.ButtonState[buttonName]){
+            // 未选中
+            case 0:
+                this.state.ButtonState[buttonName] = 1;
+                break;
+            // 选中
+            case 1:
+                this.state.ButtonState[buttonName] = 0;
+                break;
+        }
+        let arr = this.state.ButtonState
+        this.setState({ ButtonState:arr })
+    }
+    // componentDidMount() { }
 
-    componentWillReceiveProps() { }
+    // componentWillReceiveProps() { }
 
     shouldComponentUpdate(nextProps: Props, nextState: State) { return true }
 
-    componentWillUpdate() { }
+    // componentWillUpdate() { }
 
-    componentDidUpdate() { }
+    // componentDidUpdate() { }
 
-    componentWillUnmount() { }
 }
